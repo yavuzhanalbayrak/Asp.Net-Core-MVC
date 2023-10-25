4011e6732ec67a1a828b2582a43f93e2be3f9cd4
Yavuzhan Albayrak <82886846+yavuzhanalbayrak@users.noreply.github.com>
2023-10-25 23:24:45 +0300

    yeni kategoriler eklendi

--- Lezita2/Models/DbList.cs
+++ Lezita2/Models/DbList.cs
@@ -12,16 +12,16 @@ namespace Lezita2.Models
             {
                 new Product(){Name="Kola",CategoryId=1,Description="Bu ürün leziz soğuk bir koladır.",Image="/images/kola.jpg"  },
                 new Product(){Name="Fanta",CategoryId=1,Description="Bu ürün leziz soğuk bir Fantadır.",Image="/images/fanta.jpg"  },
-                new Product(){Name="Kahve",CategoryId=2,Description="Bu ürün leziz sıcacık bir kahvedir.",Image="/images/kahve.jpg"  },
+                new Product(){Name="Kahve",CategoryId=6,Description="Bu ürün leziz sıcacık bir kahvedir.",Image="/images/kahve.jpg"  },
             };
             categories = new List<Category>
             {
                 new Category(){Name="SOĞUK İÇECEKLER",Image="/images/Sogukicecek.jpg"  },
                 new Category(){Name="SICAK İÇECEKLER",Image="/images/Coffee.jpg"  },
                 new Category(){Name="BURGERLER",Image="/images/Burger.jpg"  },
-                new Category(){Name="KURABİYELER",Image="/images/Burger.jpg"  },
-                new Category(){Name="BURGERLER",Image="/images/Burger.jpg"  },
-                new Category(){Name="BURGERLER",Image="/images/Burger.jpg"  },
+                new Category(){Name="KURABİYELER",Image="/images/kurabiyeler.jpg"  },
+                new Category(){Name="KEKLER",Image="/images/kekler.jpg"  },
+                new Category(){Name="Kahveler",Image="/images/kahveler.jpg"  },
             };
         }
     }
