e33459a098de6ccb9fae88a0da8f6f129b680bfb
Yavuzhan Albayrak <82886846+yavuzhanalbayrak@users.noreply.github.com>
2023-10-25 22:06:12 +0300

    Add project files.

---
+++ Lezita2/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Lezita2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lezita2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var categoryies = DbList.categories;
+            return View(categoryies);
+        }
+    }
+}
---
+++ Lezita2/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Lezita2.Models
+{
+    public class Product
+    {
+        int Id { get; set; }
+        public static int Count { get; private set; } = 0;
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public int CategoryId { get; set; }
+        public Product()
+        {
+            Id = ++Count;
+        }
+    }
+}
