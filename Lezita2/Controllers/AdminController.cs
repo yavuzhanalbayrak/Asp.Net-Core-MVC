8ac1a93e0af5e9f92db8b7746cd4f7d063ca4171
Yavuzhan Albayrak <82886846+yavuzhanalbayrak@users.noreply.github.com>
2023-10-26 14:00:48 +0300

    admin paneli eklendi

---
+++ Lezita2/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using Lezita2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lezita2.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult GetProducts()
+        {
+            ViewBag.Categories = DbList.categories;
+            var products = DbList.products;
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult AddProducts()
+        {
+            ViewBag.Categories = DbList.categories;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddProducts(Product product)
+        {
+            DbList.products.Add(product);
+            return RedirectToAction("GetProducts");
+        }
+        public IActionResult GetCategories()
+        {
+            return View();
+        }
+
+    }
+}
